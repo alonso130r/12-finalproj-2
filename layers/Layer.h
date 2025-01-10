bea3303027a438b5a7359b68453ff8ad0f16ce69
alonso130r <131227801+alonso130r@users.noreply.github.com>
2025-01-10 13:33:26 -0500

    made stuff for adam lol

---
+++ layers/Layer.h
@@ -0,0 +1,16 @@
+//
+// Created by Vijay Goyal on 2025-01-10.
+//
+
+#ifndef INC_12_FINALPROJ_2_LAYER_H
+#define INC_12_FINALPROJ_2_LAYER_H
+
+
+
+class Layer {
+
+};
+
+#include "Layer.tpp"
+
+#endif //INC_12_FINALPROJ_2_LAYER_H
---
+++ layers/Layer.tpp
@@ -0,0 +1,5 @@
+//
+// Created by Vijay Goyal on 2025-01-10.
+//
+
+#include "Layer.h"
