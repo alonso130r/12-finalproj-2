5dabeb9a9c9dc2392363ca744d2d16b7003176d1
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2025-01-16 13:31:46 -0500

    load dataset

--- python/test.py
+++ python/test.py
@@ -1,1 +1,13 @@
-import ModularCNN
+# import ModularCNN
+import datasets
+from datasets import load_dataset
+
+ds = load_dataset("AlvaroVasquezAI/Animal_Image_Classification_Dataset")
+
+# Let's do an 80/10/10 split
+split_dataset = ds["train"].train_test_split(test_size=0.1, seed=42)
+train_dataset = split_dataset["train"]
+test_dataset = split_dataset["test"]
+
+
+print(train_dataset, test_dataset)
